eb0ef61b6bdb5c0cf7fa742f3447c477ed9db7de
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2026-06-18 22:51:46 +0000

    feat: US-008 - Fix codegen in linter.ts

--- dist/linter.mjs
+++ dist/linter.mjs
@@ -19101,7 +19101,7 @@ var builtinRegistry = {
 // linter.ts
 var __filename = fileURLToPath(import.meta.url);
 var LINTER_VERSION = true ? "0.0.1" : "dev";
-var LINTER_COMMIT = true ? "c2134e5" : "unknown";
+var LINTER_COMMIT = true ? "74b1205" : "unknown";
 var UPGRADE_URL = "https://raw.githubusercontent.com/skyrim-multiplayer/linter/main/dist/linter.mjs";
 var YARN_INSTALL_SPEC = "https://github.com/skyrim-multiplayer/linter#main";
 var getRepoRoot = () => {
