5dd84d7dae2604b64937e097ee5217c29ce25f87
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2026-07-25 23:56:19 +0000

    update

--- dist/linter.mjs
+++ dist/linter.mjs
@@ -19089,7 +19089,7 @@ var normalizeFindings = async (file, checkName, res) => {
   return findings;
 };
 var LINTER_VERSION = true ? "0.0.1" : "dev";
-var LINTER_COMMIT = true ? "1742ff7" : "unknown";
+var LINTER_COMMIT = true ? "fbea54a" : "unknown";
 var UPGRADE_URL = "https://raw.githubusercontent.com/skyrim-multiplayer/linter/main/dist/linter.mjs";
 var YARN_INSTALL_SPEC = "https://github.com/skyrim-multiplayer/linter#main";
 var getRepoRoot = () => {
